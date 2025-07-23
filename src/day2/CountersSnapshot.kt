cda9c7883e2e70e6d41228636b52aaf421520ee1
rwx788 <rodion.iafarov@jetbrains.com>
2025-07-23 11:41:25 +0200

    Implement mechanism to grab 3 values simultaneously

--- src/day2/CountersSnapshot.kt
+++ src/day2/CountersSnapshot.kt
@@ -12,7 +12,18 @@ class CountersSnapshot {
     fun incrementCounter3() = counter3.getAndIncrement()
 
     fun countersSnapshot(): Triple<Long, Long, Long> {
-        // TODO: make me atomic using the double-collect technique.
-        return Triple(counter1.get(), counter2.get(), counter3.get())
+
+        while (true) {
+            val first = counter1.get()
+            val second = counter2.get()
+            val third = counter3.get()
+
+            if (first != counter1.get()  ||
+                second != counter2.get()
+            ) continue
+
+            return Triple(first, second, third)
+        }
+
     }
 }
