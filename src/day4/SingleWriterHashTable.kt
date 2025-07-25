42973364a96080866e44adebf4cd09b72df30e0e
rwx788 <rodion.iafarov@jetbrains.com>
2025-07-25 14:10:18 +0200

    Implement concurrent hash without resize

--- src/day4/SingleWriterHashTable.kt
+++ src/day4/SingleWriterHashTable.kt
@@ -131,7 +131,7 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
                         // Mark the slot available for `put(..)`,
                         // but do not stop on this cell when searching for a key.
                         // For that, replace the key with `REMOVED_KEY`.
-                        keys[index] = REMOVED_KEY
+                        // keys[index] = REMOVED_KEY
                         // Read the value associated with the key and replace it with `null`.
                         val oldValue = values[index]
                         values[index] = null
@@ -156,10 +156,10 @@ class SingleWriterHashTable<K : Any, V : Any>(initialCapacity: Int) : HashTable<
 
 private const val MAGIC = -0x61c88647 // golden ratio
 private const val MAX_PROBES = 2 // DO NOT CHANGE THIS CONSTANT
-private val NEEDS_RESIZE = Any()
+val NEEDS_RESIZE = Any()
 
 // TODO: Once a table cell is associated with a key,
 // TODO: it should be associated with it forever.
 // TODO: This way, `remove()` should only set `null` to the value slot,
 // TODO: without replacing the key slot with `REMOVED_KEY`.
-private val REMOVED_KEY = Any()
+val REMOVED_KEY = Any()
